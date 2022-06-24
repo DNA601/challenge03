e6a39915100dc0a971d6158013a417731e5f9ca4
DNA601 <dnavarrete6001@gmail.com>
2022-06-23 23:05:32 -0700

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,27 @@
 // Assignment Code
+function generatePassword() {
+
+    var cH = ["*", "$", "@", "#", ":", "&", "?", "!", ".", "+", "?", "-", "^"];
+    var uC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    numberOfCharacters = prompt("How many characters do you want your password to contain? You are allowed a minimum of 8 and a maximum of 129 characters.");
+
+
+
+
+
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
