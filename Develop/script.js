04916b111f98732df1a10624a11e056726550a2c
DNA601 <dnavarrete6001@gmail.com>
2022-06-24 18:05:22 -0700

    choose ar least

--- Develop/script.js
+++ Develop/script.js
@@ -14,12 +14,24 @@ function generatePassword() {
     } else {
         alert("Congrats, your password will contain " + numberOfCharacters + " characters. ");
 
-
-
+    }
+    if (numberOfCharacters >= 8 || numberOfCharacters <= 128) {
+        numbers = confirm("Do you want to contain numbers?");
+        cH = confirm("Do you want to contain special characters?");
+        uC = confirm("Do you want to contain uppercase letters?");
+        lowC = confirm("Do you want to contain lowercase letters?");
 
 
     }
-}
+    if (numbers === false || cH === false || uC === false || lowC === false) {
+
+        return alert(" Choose at least one character type.");
+    }
+};
+
+
+
+
 
 var generateBtn = document.querySelector("#generate");
 
