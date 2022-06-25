c08435311356967420ef4f811e20caa6d5145ae7
DNA601 <dnavarrete6001@gmail.com>
2022-06-24 20:25:04 -0700

    gen passkey

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,63 @@
 // Assignment Code
-
+var finalP = [];
 var cH = ["*", "$", "@", "#", ":", "&", "?", "!", ".", "+", "?", "-", "^"];
 var uC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//variables for all possible combo of passwords
+var n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//variables arrays for all possible combo of passwords
 function generatePassword() {
 
     numberOfCharacters = prompt("How many characters do you want your password to contain? You are allowed a minimum of 8 and a maximum of 128 characters.");
     //pop up once button is clicked.
     if (numberOfCharacters < 8 || numberOfCharacters > 128) {
-        return alert("invalid input of characters. Try agaim");
+        return "invalid input of characters. Try agaim";
     } else {
         alert("Congrats, your password will contain " + numberOfCharacters + " characters. ");
 
     }
-    if (numberOfCharacters >= 8 || numberOfCharacters <= 128) {
+    if (numberOfCharacters >= 8 || numberOfCharacters <= 128)
+
+    {
+
+        uppercase = confirm("Do you want to contain uppercase letter?");
+
         numbers = confirm("Do you want to contain numbers?");
-        cH = confirm("Do you want to contain special characters?");
-        uC = confirm("Do you want to contain uppercase letters?");
-        lowC = confirm("Do you want to contain lowercase letters?");
 
+        lowercase = confirm("Do you want to contain lowercase letters?");
 
+        special = confirm("Do you want to contain special characters?");
+        //confirms the character type
     }
-    if (numbers === false || cH === false || uC === false || lowC === false) {
+    if (numbers === false && special === false && uppercase === false && lowercase === false) {
 
-        return alert(" Choose at least one character type.");
+        return " Choose at least one character type.";
     }
+
+
+
+    if (numbers, uppercase, lowercase, special) {
+        finalP = finalP.concat(n);
+        finalP = finalP.concat(cH);
+        finalP = finalP.concat(uC);
+        finalP = finalP.concat(lowC);
+        //this creates new arrays
+    }
+
+
+    let pass = " "
+    for (let i = 0; i < numberOfCharacters; i++) {
+        let rng = [Math.floor(Math.random() * finalP.length)];
+        pass = pass + finalP[rng];
+    }
+    //chooses a random password 
+    return pass;
+
+
+
+
+
+
+
 };
 
 
